7d703f14459fb815c99c7050ab2d122a59cf96dc
keyberth-rengel <keyberthdev@gmail.com>
2022-01-23 15:19:18 -0500

    add send email with formspree

--- js/init.js
+++ js/init.js
@@ -635,52 +635,51 @@ function iknow_tm_contact_form() {
 
   jQuery(".contact_form #send_message")
     .off()
-    .on("click", function () {
+    .on("click", async function () {
+      var url = "https://formspree.io/f/mvolzlel";
       var name = jQuery(".contact_form #name").val();
       var email = jQuery(".contact_form #email").val();
+      var number_phone = jQuery(".contact_form #phone").val();
       var message = jQuery(".contact_form #message").val();
       var subject = jQuery(".contact_form #subject").val();
       var success = jQuery(".contact_form .returnmessage").data("success");
+      var data = {
+        Name: name,
+        Email: email,
+        "Number phone": number_phone,
+        Subject: subject,
+        Message: message,
+      };
 
       jQuery(".contact_form .returnmessage").empty(); //To empty previous error/success message.
       //checking for blank fields
-      if (name === "" || email === "" || message === "") {
+      if (name === "" || email === "" || message === "" || subject === "") {
         jQuery("div.empty_notice").slideDown(500).delay(2000).slideUp(500);
       } else {
-        // Returns successful data submission message when the entered information is stored in database.
-        jQuery.post(
-          "modal/contact.php",
-          {
-            ajax_name: name,
-            ajax_email: email,
-            ajax_message: message,
-            ajax_subject: subject,
-          },
-          function (data) {
-            jQuery(".contact_form .returnmessage").append(data); //Append returned message to message paragraph
-
-            if (
-              jQuery(".contact_form .returnmessage span.contact_error").length
-            ) {
-              jQuery(".contact_form .returnmessage")
-                .slideDown(500)
-                .delay(2000)
-                .slideUp(500);
-            } else {
-              jQuery(".contact_form .returnmessage").append(
-                "<span class='contact_success'>" + success + "</span>"
-              );
-              jQuery(".contact_form .returnmessage")
-                .slideDown(500)
-                .delay(4000)
-                .slideUp(500);
-            }
-
-            if (data === "") {
-              jQuery("#contact_form")[0].reset(); //To reset form fields on success
-            }
-          }
-        );
+        axios
+          .post(url, {
+            method: "post",
+            body: data,
+            headers: {
+              Accept: "application/json",
+            },
+          })
+          .then((response) => {
+            jQuery(".contact_form .returnmessage").append(
+              "<span class='contact_success'>" + success + "</span>"
+            );
+            jQuery(".contact_form .returnmessage")
+              .slideDown(500)
+              .delay(4000)
+              .slideUp(500);
+            jQuery("#contact_form")[0].reset();
+          })
+          .catch((error) => {
+            jQuery(".contact_form .returnmessage")
+              .slideDown(500)
+              .delay(2000)
+              .slideUp(500);
+          });
       }
       return false;
     });
